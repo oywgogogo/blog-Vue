4c666751df1a2d4972bf796fd1153709db50c01d
oywgogogo <2226477173@qq.com>
2022-04-11 10:51:13 +0800

    修改生产环境配置

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:8888/"'
+  BASE_API: '"89.47.166.199:8888/"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,5 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: 'production"',
+  BASE_API: '"89.47.166.199:8888/"'
 }
